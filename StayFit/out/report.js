5b5c7639dc31ba13a52387b1f772c1088e17b375
Sushrut Naik <sushrutn@nitorinfotech.com>
2016-06-20 20:08:51 +0530

    First Commit

---
+++ StayFit/out/report.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("LogIn_Test.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login Action",
+  "description": "",
+  "id": "login-action",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Successful Login with Valid Credentials",
+  "description": "",
+  "id": "login-action;successful-login-with-valid-credentials",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User Navigate to LogIn Page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "User enters UserName and Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "Message displayed Login Successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Test_Steps.user_is_on_Home_Page()"
+});
+formatter.result({
+  "duration": 37488134332,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Test_Steps.user_Navigate_to_LogIn_Page()"
+});
+formatter.result({
+  "duration": 7680250199,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Test_Steps.user_enters_UserName_and_Password()"
+});
+formatter.result({
+  "duration": 2134395038,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Test_Steps.message_displayed_Login_Successfully()"
+});
+formatter.result({
+  "duration": 233812,
+  "status": "passed"
+});
+});
